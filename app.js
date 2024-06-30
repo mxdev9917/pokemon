9b4cb6760fbc3e5121936f0f9fb3d4d96b7935bc
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2024-06-30 16:55:31 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// // https://pokeapi.co/api/v2/pokemon/
+// // https://pokeapi.co/api/v2/pokemon/weedle
+
+const formEle = document.getElementById('form');
+const contentEle = document.querySelector("#show-item")
+const query=document.querySelector('#query')
+
+async function getpokamon(search) {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+        const data = await res.json()
+        const pEle = document.createElement('p')
+        pEle.textContent = data.name;
+        const imgEle = document.createElement('img')
+        imgEle.src = data.sprites.front_default
+        contentEle.append(pEle)
+        pEle.append(imgEle)
+    } catch (error) {
+        console.log('Error', error);
+    }
+
+}
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        for (const item of data.results) {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${item.name}`)
+                .then(ress => {
+                    return ress.json()
+                }).then(data => {
+
+                    const pEle = document.createElement('p')
+                    pEle.textContent = data.name;
+                    const imgEle = document.createElement('img')
+                    imgEle.src = data.sprites.front_default
+                    contentEle.append(pEle)
+                    pEle.append(imgEle)
+                })
+        }
+    })
+    .catch(err => {
+        console.log("Error", err);
+    })
+
+
+formEle.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const search = formEle.elements.query.value;
+    if (search) {
+          query.value=null
+        getpokamon(search)
+      
+        contentEle.innerHTML = ""
+    } else {
+        location.reload();
+
+    }
+})
